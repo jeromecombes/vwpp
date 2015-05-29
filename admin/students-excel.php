14d73e06468288088f0e69f69c9295fb4e7de92d
Jérôme Combes <jerome@planningbiblio.fr>
2015-05-29 17:52:11 +0200

    Ajoute le champs TIN dans les exports des infos générales

--- admin/students-excel.php
+++ admin/students-excel.php
@@ -1,4 +1,5 @@
 <?php
+// Last update : 2015-05-29
 // ini_set('display_errors',1);
 // ini_set('error_reporting',E_ALL);
 
@@ -46,13 +47,13 @@ else{
 
 
 $fields=Array ("lastname","firstname","gender","homeInstitution","semestersJoin","frenchUniv","frenchNumber",
-  "citizenship1","citizenship2","dob","placeOB","countryOB","email","cellphone","contactlast",
+  "citizenship1","citizenship2","dob","placeOB","countryOB","tin","email","cellphone","contactlast",
   "contactfirst","street","city","zip","state","country","contactemail","contactphone","contactmobile",
   "h_address","h_zipcode","h_city","h_phonenumber","h_lastname","h_firstname","h_cellphone","h_email",
   "h_lastname2","h_firstname2","h_cellphone2","h_email2");
 
 $line1=Array ("Lastname","Firstname","Gender","Home Institution","Semesters","French Univ.","French Univ. Number",
-  "Citizenship1","Citizenship2","DOB","Place OB","Country OB","Email","Cellphone","Contact Lastname",
+  "Citizenship1","Citizenship2","DOB","Place OB","Country OB","TIN","Email","Cellphone","Contact Lastname",
   "Contact Firstname","C Street","C City","C Zip","C State","C Country","C Email","C Phone","C Mobile",
   "Address FR","Zip code FR","City FR","Phone numer FR","H Lastname","H Firstname","H cellphone","H email",
   "H Lastname 2","H Firstname 2","H Cellphone 2","H Email 2");
