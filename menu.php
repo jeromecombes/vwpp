78be69b986152e574c1473c510f439da720c6f34
Jérôme Combes <jerome@planningbiblio.fr>
2016-01-07 23:03:51 +0100

    Amélioration du formulaire de voyage, suppression fichiers grades inutiles

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <?php
-// Last update : 2015-10-19
+// Last update : 2016-01-07
 
 $post['semester']=filter_input(INPUT_POST,"semester",FILTER_SANITIZE_STRING);
 $get['semester']=filter_input(INPUT_GET,"semester",FILTER_SANITIZE_STRING);
@@ -41,6 +41,7 @@ if(array_key_exists("semester",$_SESSION['vwpp']) or $post['semester'] or count(
   <li id='li4' class='ui-state-default ui-corner-top' $displayEval ><a href='eval_index.php'>Evaluations</a></li>
   <li id='li8' class='ui-state-default ui-corner-top'><a href='documents.php'>Documents</a></li>
   <li id='li9' class='ui-state-default ui-corner-top'><a href='schedule.php'>Schedule</a></li>
+  <li id='li10' class='ui-state-default ui-corner-top'><a href='trip_index.php'>Trip</a></li>
   <li id='li6' class='ui-state-default ui-corner-top'><a href='myAccount.php'>My Account</a></li>
 </ul>
 EOD;
